40b13d9a45ee81bea70baad58000d09de6b536a4
Alexandre Amory <alexandre.amory@santannapisa.it>
2022-07-15 16:35:23 +0200

    going back to Count_Time in rt_dag to make it compatible with the sched_deadline and the power flow, which are both time based, not tick based

--- tools/rt-dag/rt-dag/dag.h
+++ tools/rt-dag/rt-dag/dag.h
@@ -7,7 +7,6 @@
 #define MAX_OUT_EDGES_PER_TASK 2
 #define MAX_IN_EDGES_PER_TASK 2
 #define MAX_MSG_LEN 256
-<<<<<<< HEAD
 #define REPETITIONS 5 // the number of iterations of the complete DAG
 #define DAG_PERIOD 1'000'000 // in us 
 #define DAG_DEADLINE DAG_PERIOD // usually is the same as dag period, but not necessarly
@@ -17,18 +16,6 @@ const unsigned tasks_wcet[N_TASKS] = {50'000,500'000,200'000,50'000}; // in us.
 // make sure that the sum of the longest path must be <= DAG_DEADLINE since
 // this consistency is not done here !!!
 const unsigned tasks_rel_deadline[N_TASKS] = {100'000,800'000,800'000,100'000}; // in us
-=======
-#define REPETITIONS 50 // the number of iterations of the complete DAG
-#define DAG_PERIOD 10'000 // in us 
-#define DAG_DEADLINE DAG_PERIOD // usually is the same as dag period, but not necessarly
-// The actual task computation workload in 'ticks'. a tick is equivalent to a simple logic operation.
-// Note that tick is not time ! it's a kind of workload unit
-const unsigned tasks_wcet[N_TASKS] = {50'000,500'000,200'000,50'000}; // in ticks. 
-// The relative deadline of each task.
-// make sure that the sum of the longest path must be <= DAG_DEADLINE since
-// this consistency is not done here !!!
-const unsigned tasks_rel_deadline[N_TASKS] = {1'000,8'000,8'000,1'000}; // in us
->>>>>>> including rt-dag demo results running odroid-xu4
 // pin threads/processes onto the specified cores
 // the values are the cpu ids. mask is currently not supported
 const unsigned task_affinity[N_TASKS] = {1,2,3,1};
@@ -41,10 +28,6 @@ const unsigned adjacency_matrix[N_TASKS][N_TASKS] = {
     {0, 0, 0, 0},
 };
 
-// TODO: extend the data structure to set the frequency of the islands
-// https://www.thinkwiki.org/wiki/How_to_make_use_of_Dynamic_Frequency_Scaling
-// TODO: set task affinity w cpu_set_t
 // TODO: set task scheduling attributes w struct sched_attr
-
 
 #endif // DAG_H_
