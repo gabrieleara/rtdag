9a183c50338ea55329de50ae98514fc367c08baa
Tommaso Cucinotta <tommaso.cucinotta@santannapisa.it>
2023-06-11 15:12:33 +0200

    hellogauss.sh: use llvm-omp-device-info to show device numbers seen by OMP

--- src/hellogauss.sh
+++ src/hellogauss.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+llvm-omp-device-info-15
+
+echo
+
 clang-15 -O3 hellogauss.c -o hellogauss -fopenmp -fopenmp-targets=nvptx64-nvidia-cuda,x86_64
 
 OMP_TARGET_OFFLOAD=MANDATORY ./hellogauss
